8ca3fa7c3aa14a71efa2636036b2492fa6dac70a
Donald <dnldcode@gmail.com>
2020-07-14 19:54:58 +0200

    mrc created for Message

---
+++ app/Message.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'chat_id', 'user_id', 'message'
+    ];
+}
