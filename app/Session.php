ea0ecb2ccf6e0010af6aef806d3900643592e045
Donald <dnldcode@gmail.com>
2020-07-14 19:59:10 +0200

    m created for Session

---
+++ app/Session.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Session extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'access_token',
+    ];
+}
