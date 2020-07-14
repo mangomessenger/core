07bfc9e2a4fc3b5525a621422dfbee4a59095a36
Donald <dnldcode@gmail.com>
2020-07-14 19:56:53 +0200

    m created for Message

---
+++ database/migrations/2020_07_14_175558_create_chat_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chat_id')->constrained('chats');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_members');
+    }
+}
