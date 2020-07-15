178305f283a5a5a9e2b237f7f3defb35c299fbb9
Donald <dnldcode@gmail.com>
2020-07-15 22:16:38 +0200

    Model Service created

---
+++ app/Services/ModelService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+abstract class ModelService implements ApiService
+{
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function create(array $input)
+    {
+        return $this->model->create($input);
+    }
+
+    public function find($id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function delete($id)
+    {
+        return $this->find($id)->delete();
+    }
+
+    public function update($id, array $input)
+    {
+        return $this->find($id)->update($input);
+    }
+}
