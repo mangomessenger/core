3f4884ec311d083b210dcb5ce39702f85302863e
Donald <dnldcode@gmail.com>
2020-07-18 00:45:06 +0200

    Unneccessary middlewares deleted

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,16 +30,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-        'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
-
         'api' => [
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\JsonMiddleware::class,
@@ -56,14 +46,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'jwt-auth' => \App\Http\Middleware\JWTAuthenticate::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'throttle' => ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-//        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-//        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-//        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-//        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-//        'auth' => \App\Http\Middleware\Authenticate::class,
     ];
 }
