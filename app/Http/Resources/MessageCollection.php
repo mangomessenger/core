4258416de016ccd5eb0c2417eb3ddcf0bdec1983
Donald <dnldcode@gmail.com>
2020-07-19 23:41:30 +0200

    Collections update

--- app/Http/Resources/MessageCollection.php
+++ app/Http/Resources/MessageCollection.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ChatCollection extends ResourceCollection
+class MessageCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return AnonymousResourceCollection
      */
     public function toArray($request)
     {
-        return ChatResource::collection($this->collection);
+        return MessageResource::collection($this->collection);
     }
 }
