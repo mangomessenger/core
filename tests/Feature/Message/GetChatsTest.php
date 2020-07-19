f518bfdc985ad21eaeb75768f6c110f445fe12de
Donald <dnldcode@gmail.com>
2020-07-20 01:12:19 +0200

    Tests created for getChats action

---
+++ tests/Feature/Message/GetChatsTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature\Message;
+
+use App\Chat;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class GetChatsTest extends TestCase
+{
+    use DatabaseMigrations, RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh');
+    }
+
+    public function test_get_chats_returns_204_on_success()
+    {
+        $user = factory(User::class)->create();
+
+        $token = auth()->login($user);
+
+        $this->withHeader('Authorization', "Bearer $token")
+            ->json('GET', 'chats/',)
+            ->assertStatus(200)
+            ->assertJsonStructure([]);
+    }
+
+    public function test_send_message_needs_token()
+    {
+        $this->json('GET', 'chats/', [
+            "chat_id" => 1,
+            "message_id" => 1
+        ])
+            ->assertStatus(401)
+            ->assertJson([
+                'type' => 'TOKEN_ABSENT',
+            ])->assertJsonStructure([
+                'type',
+                'message',
+                'status',
+            ]);
+    }
+}
