2c5728d7821575c9d3ccf53248d30a5a090d4453
Donald <dnldcode@gmail.com>
2020-07-19 19:36:09 +0200

    Phone number added to output

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,6 +17,7 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'phone_number' => $this->phone_number,
         ];
     }
 }
