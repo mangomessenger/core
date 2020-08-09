1595b4dc122723d36c21e4841bf64445648b5217
Donald <dnldcode@gmail.com>
2020-08-09 07:53:33 +0200

    UserSeeder added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(ChatTypeSeeder::class);
+        $this->call([
+            ChatTypeSeeder::class,
+            UserSeeder::class
+        ]);
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\User::create([
+            'name' => 'Donald',
+            'username' => NULL,
+            'phone_number' => '+380123123123',
+            'country_code' => 'UA',
+        ]);
+    }
+}
