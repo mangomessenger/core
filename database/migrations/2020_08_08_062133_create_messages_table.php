aa2caa832108c8fbf9967523aafc0c9040574969
Donald <dnldcode@gmail.com>
2020-08-09 07:16:39 +0200

    Foreign constaintes removed

--- database/migrations/2020_08_08_062133_create_messages_table.php
+++ database/migrations/2020_08_08_062133_create_messages_table.php
@@ -24,21 +24,6 @@ class CreateMessagesTable extends Migration
             $table->string('message');
             $table->boolean('is_read')->default(false);
             $table->timestamps();
-
-            $table->foreign('chat_id', 'messages_chat_id_foreign_direct')
-                ->references('id')
-                ->on('direct_chats')
-                ->onDelete('cascade');
-
-            $table->foreign('chat_id', 'messages_chat_id_foreign_channels')
-                ->references('id')
-                ->on('channels')
-                ->onDelete('cascade');
-
-            $table->foreign('chat_id', 'messages_chat_id_foreign_groups')
-                ->references('id')
-                ->on('groups')
-                ->onDelete('cascade');
         });
 
         Schema::table('messages', function (Blueprint $table) {
--- database/migrations/2020_08_08_063810_create_chat_members_table.php
+++ database/migrations/2020_08_08_063810_create_chat_members_table.php
@@ -20,21 +20,6 @@ class CreateChatMembersTable extends Migration
                 ->constrained('users')
                 ->onDelete('cascade');
             $table->timestamps();
-
-            $table->foreign('chat_id', 'chat_members_chat_id_foreign_direct')
-                ->references('id')
-                ->on('direct_chats')
-                ->onDelete('cascade');
-
-            $table->foreign('chat_id', 'chat_members_chat_id_foreign_channels')
-                ->references('id')
-                ->on('channels')
-                ->onDelete('cascade');
-
-            $table->foreign('chat_id', 'chat_members_chat_id_foreign_groups')
-                ->references('id')
-                ->on('groups')
-                ->onDelete('cascade');
         });
     }
 
