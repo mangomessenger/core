632bcc08b73d6f174c2186149d5e95bd97db5c59
Donald <dnldcode@gmail.com>
2020-08-11 21:56:13 +0200

    Basic MessagePolicy created

---
+++ app/Policies/MessagePolicy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Message;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class MessagePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine if the given message can be accessed by the user.
+     *
+     * @param User $user
+     * @param Message $message
+     * @return bool
+     */
+    public function access(User $user, Message $message)
+    {
+        return $message->chat()->members->contains($user);
+    }
+
+    /**
+     * Determine if the given message can be updated by the user.
+     *
+     * @param User $user
+     * @param Message $message
+     * @return bool
+     */
+    public function update(User $user, Message $message)
+    {
+        return $message->sender->is($user);
+    }
+
+    /**
+     * Determine if the given message can be deleted by the user.
+     *
+     * @param User $user
+     * @param Message $message
+     * @return bool
+     */
+    public function delete(User $user, Message $message)
+    {
+        return $message->sender->is($user)
+            && $message->created_at > Carbon::now()->subDays(1); // After 1 day message could not be deleted
+    }
+}
